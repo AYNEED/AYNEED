029d1b7d2992275653876ba1423e19cc0bb11475
Kirill Neruchev <kirill@neruchev.com>
2020-06-17 18:41:47 +0200

    fix(Main): restore Main route

--- frontend-web/src/components/routes/Main.tsx
+++ frontend-web/src/components/routes/Main.tsx
@@ -1,42 +1,7 @@
 import React from 'react';
-import { useQuery } from '@apollo/react-hooks';
-import { gql } from 'apollo-boost';
 
 import { Page } from 'src/components/wrappers/Page';
 
-const EXCHANGE_RATES = gql`
-  {
-    rates(currency: "USD") {
-      currency
-      rate
-    }
-  }
-`;
-
-type Rates = {
-  rates: { currency: string; rate: string }[];
-};
-
-const Main: React.FC = () => {
-  const { loading, error, data } = useQuery<Rates>(EXCHANGE_RATES);
-
-  if (loading) {
-    return <p>Loading...</p>;
-  }
-
-  if (error || !data) {
-    return <p>Error</p>;
-  }
-
-  return (
-    <Page title>
-      {data.rates.map(({ currency, rate }) => (
-        <p key={currency}>
-          {currency}: {rate}
-        </p>
-      ))}
-    </Page>
-  );
-};
+const Main: React.FC = () => <Page title />;
 
 export default Main;
