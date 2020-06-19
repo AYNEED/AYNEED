2af5f49b44b3916da91faa68870c6e5992c91616
Kirill Neruchev <kirill@neruchev.com>
2020-06-19 09:04:44 +0200

    fix(types): fix types & add 'build' to esignore

--- shared/src/i18n/index.ts
+++ shared/src/i18n/index.ts
@@ -8,7 +8,7 @@ export const locales = {
   },
 } as const;
 
-export const LOCALES: Locales[] = Object.keys(locales) as any[];
+export const LOCALES: Locales[] = Object.keys(locales) as never[];
 export const defaultLocale = 'rus';
 const defaultDictionary = locales[defaultLocale].dictionary;
 
