afa24da0f5cccf50787bfe932151b4cd2c71b646
Kirill Neruchev <kirill@neruchev.com>
2020-06-20 14:08:14 +0200

    fix(backend): add playground theme support

--- backend-api/src/index.ts
+++ backend-api/src/index.ts
@@ -1,12 +1,23 @@
 import 'src/utils/env';
 
 import { ApolloServer } from 'apollo-server';
+import { Theme } from '@apollographql/graphql-playground-html/dist/render-playground-page';
 
 import { typeDefs } from 'src/typeDefs';
 import { resolvers } from 'src/resolvers';
 import { version } from 'package.json';
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  playground: {
+    settings: {
+      'editor.theme': process.env
+        .AYNEED_APOLLO_SERVER_PLAYGROUND_THEME as Theme,
+      'request.credentials': 'same-origin',
+    },
+  },
+});
 
 server.listen({ port: process.env.AYNEED_BACKEND_API_PORT }).then(({ url }) => {
   console.log(`🚀 Server ${version} ready at ${url}`);
