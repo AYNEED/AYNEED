15735aadd20eb4209398a55aadc058e8facb8abc
Kirill Neruchev <kirill@neruchev.com>
2020-06-21 12:02:31 +0200

    fix(project): fix coding style

--- backend-api/src/index.ts
+++ backend-api/src/index.ts
@@ -7,21 +7,21 @@ import { typeDefs } from 'src/typeDefs';
 import { resolvers } from 'src/resolvers';
 import { version } from 'package.json';
 
+const theme = process.env.AYNEED_APOLLO_SERVER_PLAYGROUND_THEME as Theme;
+const port = process.env.AYNEED_BACKEND_API_PORT;
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   playground: {
     settings: {
-      'editor.theme': process.env
-        .AYNEED_APOLLO_SERVER_PLAYGROUND_THEME as Theme,
+      'editor.theme': theme,
       'request.credentials': 'same-origin',
     },
   },
 });
 
-server
-  .listen({ port: process.env.AYNEED_BACKEND_API_PORT })
-  .then(({ url, subscriptionsUrl }) => {
-    console.log(`🚀 Server ${version} ready at ${url}`);
-    console.log(`Subscriptions ready at ${subscriptionsUrl}`);
-  });
+server.listen({ port }).then(({ url, subscriptionsUrl }) => {
+  console.log(`🚀 Server ${version} ready at ${url}`);
+  console.log(`Subscriptions ready at ${subscriptionsUrl}`);
+});
