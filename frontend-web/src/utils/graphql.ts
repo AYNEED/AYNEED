1cbb46cd1b4e1f1a90d12b8f285c83e8eb337926
Kirill Neruchev <kirill@neruchev.com>
2020-06-28 20:01:23 +0200

    fix(graphql): add cache-control

--- frontend-web/src/utils/graphql.ts
+++ frontend-web/src/utils/graphql.ts
@@ -1,4 +1,4 @@
-import ApolloClient from 'apollo-client';
+import ApolloClient, { DefaultOptions } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { PersistedData, PersistentStorage } from 'apollo-cache-persist/types';
 import { split } from 'apollo-link';
@@ -20,7 +20,7 @@ localForage.config({
   name: 'ayneed',
   driver: process.env.NODE_ENV === 'test' ? driverTests : driver,
   version: 2,
-  storeName: 'keyvaluepairs',
+  storeName: process.env.REACT_APP_VERSION,
 });
 
 export const cache = new InMemoryCache({});
@@ -50,7 +50,14 @@ const link = split(
   httpLink
 );
 
+const defaultOptions: DefaultOptions = {
+  watchQuery: {
+    fetchPolicy: 'cache-and-network',
+  }
+};
+
 export const client = new ApolloClient({
   cache,
   link,
+  defaultOptions,
 });
