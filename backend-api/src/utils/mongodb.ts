99d18ec5d48c1daa3a4e8361d5a70c692bc67a12
Kirill Neruchev <kirill@neruchev.com>
2020-06-28 16:36:40 +0200

    fix(db): update user model

--- backend-api/src/utils/mongodb.ts
+++ backend-api/src/utils/mongodb.ts
@@ -1,4 +1,4 @@
-import mongoose, { ConnectionOptions } from 'mongoose';
+import mongoose, { ConnectionOptions, SchemaOptions } from 'mongoose';
 
 export const name = process.env.AYNEED_BACKEND_MONGO_DB_NAME || '';
 export const host = process.env.AYNEED_BACKEND_MONGO_DB_HOST || '';
@@ -6,6 +6,12 @@ export const host = process.env.AYNEED_BACKEND_MONGO_DB_HOST || '';
 export const options: ConnectionOptions = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+};
+
+export const schemaOptions: SchemaOptions = {
+  id: true,
+  versionKey: false,
+  timestamps: true,
 };
 
 export const connect = async (): Promise<void> => {
