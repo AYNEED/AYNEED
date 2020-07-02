cfa77bce1cb55e3dab632bf9c273ff15385e481f
Kirill Neruchev <kirill@neruchev.com>
2020-07-02 14:57:25 +0200

    fix(pages): fix Main page

--- frontend-web/src/routes/Main/index.tsx
+++ frontend-web/src/routes/Main/index.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
 import { Page } from 'src/components/wrappers/Page';
+import Logo from 'src/components/ui/Logo';
 
-const Main: React.FC = () => <Page title />;
+const Main: React.FC = () => (
+  <Page>
+    <Logo />
+
+    <p>Объединяет людей для создания бизнеса</p>
+  </Page>
+);
 
 export default Main;
