77cf61f4d711232a2b39f40cf22cf84b4db38e11
Kirill Neruchev <kirill@neruchev.com>
2020-07-02 14:08:25 +0200

    fix(icons): add new icons

---
+++ frontend-web/src/components/icons/Desktop.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { COLOR } from 'src/constants/colors';
+
+type Props = {
+  fill?: COLOR;
+};
+
+export const Desktop: React.FC<Props> = ({ fill = COLOR.WHITE }) => (
+  <svg width="10" height="6" viewBox="0 0 10 6" fill="none">
+    <path
+      d="M10 0.750001C10 0.335787 9.6802 7.15256e-07 9.28571 7.15256e-07L0.714286 0C0.319797 0 3.40598e-07 0.335786 3.40598e-07 0.75L0 5.25C0 5.66421 0.319797 6 0.714286 6H9.28571C9.6802 6 10 5.66422 10 5.25V0.750001Z"
+      fill={fill}
+    />
+  </svg>
+);
