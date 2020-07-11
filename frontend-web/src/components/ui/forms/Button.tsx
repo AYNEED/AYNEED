ac2e4f7d569820baa33de3acd9cdc658c53d1f05
seliana <cany96@bk.ru>
2020-07-12 01:18:17 +0300

    feat(button): add hover

--- frontend-web/src/components/ui/forms/Button.tsx
+++ frontend-web/src/components/ui/forms/Button.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FelaComponent } from 'react-fela';
+import { useFela } from 'react-fela';
 
 import { Styles } from 'src/utils/fela';
 import { Link } from 'src/components/ui/Link';
@@ -14,17 +15,7 @@ type ButtonSubmitProps = {
   disabled?: boolean;
 };
 
-const styles: Styles<'button' | 'text'> = {
-  button: {
-    position: 'relative',
-    height: 50,
-    width: '100%',
-    backgroundColor: COLOR.TRANSPARENT,
-    border: `2px solid ${COLOR.PRIMARY_500}`,
-    display: 'flex !important',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
+const styles: Styles<'text'> = {
   text: {
     color: COLOR.PRIMARY_500,
     textTransform: 'uppercase',
@@ -32,22 +23,66 @@ const styles: Styles<'button' | 'text'> = {
     fontWeight: 600,
     fontSize: '16px',
     lineHeight: '20px',
+    zIndex: 10,
   },
 };
 
-export const ButtonLink: React.FC<ButtonLinkProps> = ({ url, children }) => {
-  const onMouseOverHandler = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>): void => {
-    // const x = 
+const buttonRule: object = () => ({
+  position: 'relative',
+  height: 50,
+  width: '100%',
+  backgroundColor: COLOR.TRANSPARENT,
+  border: `2px solid ${COLOR.PRIMARY_500}`,
+  display: 'flex !important',
+  justifyContent: 'center',
+  alignItems: 'center',
+  cursor: 'pointer',
+  overflow: 'hidden',
+})
 
-    console.log(event.pageX)
-    // console.log(linkRef.offsetTop)
+const rippleHover: string = ("position: absolute; width: 100px; height: 100px; borderRadius: 10px; background: linear-gradient(0deg, #D4EFDF 28.02%, rgba(212, 239, 223, 0) 100%); animation: .5s rippleExpand ease-out; zIndex: 5;")
+
+// const keyframe: string = (`
+//   @keyframes rippleExpand{
+//     0% { transform: scale(0); opacity: 1; }
+//     100% { transform: scale(var(--scale)); opacity: 0; }
+//   }
+// `)
+
+export const ButtonLink: React.FC<ButtonLinkProps> = ({ url, children }) => {
+  const { css } = useFela()
+
+  const buttonLink: React.RefObject<HTMLDivElement> = React.createRef()
+
+  const onMouseOverHandler = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>): void => {
+
+    if (buttonLink.current !== null) {
+      const y: string | number = event.pageY - buttonLink.current.offsetTop
+      const x: string | number = event.pageX - buttonLink.current.offsetLeft
+      const w: any = buttonLink.current.offsetWidth / 2
+      
+      const ripple: HTMLElement = document.createElement('span')
+      ripple.setAttribute('style', rippleHover)
+      ripple.style.top = y + "px"
+      ripple.style.left = x + "px"
+      ripple.style.setProperty('--scale', w);
+      
+      buttonLink.current.appendChild(ripple)
+    
+      setTimeout(() => {
+        if (ripple !== null && ripple.parentNode !== null) {
+          ripple.parentNode.removeChild(ripple)
+        }
+      }, 500)
+    }
   }
+
 
   return(
     <Link onMouseOver={onMouseOverHandler} url={url}>
-      <FelaComponent style={styles.button}>
+      <div className={css(buttonRule)} ref={buttonLink}>
         <FelaComponent style={styles.text}>{children}</FelaComponent>
-      </FelaComponent>
+      </div>
     </Link>
   )
 };
