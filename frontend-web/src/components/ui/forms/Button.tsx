83615e4dd25dc3a8d8fe4ed7745eb1dec99f9d4b
seliana <cany96@bk.ru>
2020-07-11 21:13:37 +0300

    client(feat): button style added

--- frontend-web/src/components/ui/forms/Button.tsx
+++ frontend-web/src/components/ui/forms/Button.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import { FelaComponent } from 'react-fela';
 
+import { Styles } from 'src/utils/fela';
 import { Link } from 'src/components/ui/Link';
 import { Scheme } from 'src/navigation';
+import { COLOR } from 'src/constants/colors';
 
 type ButtonLinkProps = {
   url: Scheme;
@@ -11,11 +14,43 @@ type ButtonSubmitProps = {
   disabled?: boolean;
 };
 
-export const ButtonLink: React.FC<ButtonLinkProps> = ({ url, children }) => (
-  <Link url={url}>
-    <button type="button">{children}</button>
-  </Link>
-);
+const styles: Styles<'button' | 'text'> = {
+  button: {
+    position: 'relative',
+    height: 50,
+    width: '100%',
+    backgroundColor: COLOR.TRANSPARENT,
+    border: `2px solid ${COLOR.PRIMARY_500}`,
+    display: 'flex !important',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: COLOR.PRIMARY_500,
+    textTransform: 'uppercase',
+    fontStyle: 'normal',
+    fontWeight: 600,
+    fontSize: '16px',
+    lineHeight: '20px',
+  },
+};
+
+export const ButtonLink: React.FC<ButtonLinkProps> = ({ url, children }) => {
+  const onMouseOverHandler = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>): void => {
+    // const x = 
+
+    console.log(event.pageX)
+    // console.log(linkRef.offsetTop)
+  }
+
+  return(
+    <Link onMouseOver={onMouseOverHandler} url={url}>
+      <FelaComponent style={styles.button}>
+        <FelaComponent style={styles.text}>{children}</FelaComponent>
+      </FelaComponent>
+    </Link>
+  )
+};
 
 export const ButtonSubmit: React.FC<ButtonSubmitProps> = ({
   disabled,
