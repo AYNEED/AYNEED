59be083ccf780d8e6c23680399bd7d2f31fa63d1
seliana <cany96@bk.ru>
2020-07-12 03:01:09 +0300

    feat(main): main page layout

--- frontend-web/src/routes/Main/index.tsx
+++ frontend-web/src/routes/Main/index.tsx
@@ -1,4 +1,9 @@
 import React from 'react';
+import { FelaComponent } from 'react-fela';
+import { Styles } from 'src/utils/fela';
+// import { useFela } from 'react-fela'
+
+import { COLOR } from 'src/constants/colors';
 
 import { Page } from 'src/components/wrappers/Page';
 import { ButtonLink } from 'src/components/ui/forms/Button';
@@ -13,33 +18,61 @@ import { Link } from 'src/components/ui/Link';
 
 const Logo = React.lazy(() => import('src/components/ui/Logo'));
 
+
+const styles: Styles<'pageWrapper' | 'title' | 'content'> = {
+  pageWrapper: {
+    width: '100%',
+    height: '100%',
+    backgroundColor: COLOR.BACKGROUND_GRAY,
+    padding: '308px 419px 277px 350px',
+    paddingLeft: '350px',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+  },
+  title: {
+    width: '95px',
+    color: COLOR.GRAY,
+    marginTop: '42px',
+  },
+  content: {
+    position: 'relative',
+    marginTop: '60px',
+    display: 'flex',
+    alignItems: 'flex-start',
+  }
+}
+
 const Main: React.FC = () => (
   <Page>
-    <Logo />
+    <FelaComponent style={styles.pageWrapper}>
+      <Logo />
 
-    <p>Объединяет людей для создания бизнеса</p>
+      <FelaComponent style={styles.title}>Объединяет людей для создания бизнеса</FelaComponent>
 
-    <ButtonLink url={{ scheme: ROUTES.FEED }}>
-      <Msg id="web.routes.Main.button_start" />
-    </ButtonLink>
+      <FelaComponent style={styles.content}>
+        <ButtonLink url={{ scheme: ROUTES.FEED }}>
+          <Msg id="web.routes.Main.button_start" />
+        </ButtonLink>
 
-    <Ball id="web.routes.Main.ball_idea">
-      <Idea />
-    </Ball>
+        <Ball id="web.routes.Main.ball_idea">
+          <Idea />
+        </Ball>
 
-    <Ball id="web.routes.Main.ball_command">
-      <Command />
-    </Ball>
+        <Ball id="web.routes.Main.ball_command">
+          <Command />
+        </Ball>
 
-    <Ball id="web.routes.Main.ball_investments">
-      <Investments />
-    </Ball>
+        <Ball id="web.routes.Main.ball_investments">
+          <Investments />
+        </Ball>
 
-    <Rocket />
-
-    <Link url={{ scheme: ROUTES.SIGN_IN_EMAIL }}>
-      <Msg id="web.routes.Main.button_sign_in" />
-    </Link>
+        <Rocket />
+      </FelaComponent>
+      <Link url={{ scheme: ROUTES.SIGN_IN_EMAIL }}>
+        <Msg id="web.routes.Main.social_media" />
+      </Link>
+    </FelaComponent>
   </Page>
 );
 
