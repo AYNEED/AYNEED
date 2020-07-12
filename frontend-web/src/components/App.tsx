8024ff959eacc6e823dc9ae46bc82248875f7190
seliana <cany96@bk.ru>
2020-07-12 03:00:35 +0300

    feat(app): add main styles

--- frontend-web/src/components/App.tsx
+++ frontend-web/src/components/App.tsx
@@ -1,17 +1,39 @@
 import React from 'react';
 import { RendererProvider } from 'react-fela';
+import { createRenderer } from 'fela'
 
-import { renderer } from 'src/utils/fela';
 import { Router } from 'src/components/wrappers/Router';
 import { IntlProvider } from 'src/i18n/IntlProvider';
 import { ApolloProvider } from 'src/components/wrappers/ApolloProvider';
 
-export const App: React.FC = () => (
-  <ApolloProvider>
-    <IntlProvider>
-      <RendererProvider renderer={renderer}>
-        <Router />
-      </RendererProvider>
-    </IntlProvider>
-  </ApolloProvider>
-);
+export const App: React.FC = () => {
+  const renderer = createRenderer()
+  renderer.renderStatic(`
+    * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+    }
+
+    body, html {
+      width: 100%;
+      height: 100%;
+    }
+
+    #root{
+      position: relative;
+      width: 100%;
+      height: 100%;
+    }
+  `)
+
+  return(
+    <ApolloProvider>
+      <IntlProvider>
+        <RendererProvider renderer={renderer}>
+          <Router />
+        </RendererProvider>
+      </IntlProvider>
+    </ApolloProvider>
+  )
+};
