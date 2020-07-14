de504cb941e87c6539b4929d8ab8c633a8e550a1
Kirill Neruchev <kirill@neruchev.com>
2020-07-14 15:55:29 +0200

    feat(tests): add client tests

--- frontend-web/src/components/App.test.tsx
+++ frontend-web/src/components/App.test.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, wait } from '@testing-library/react';
 
 import { App } from 'src/components/App';
 
-test('render without errors', () => {
-  const { getByText } = render(<App />);
-  const title = getByText(/TODO: show preloader/i);
+describe('Test render', () => {
+  test('render without errors', async () => {
+    const { getByText } = render(<App />);
+    await wait();
 
-  expect(title).toBeInTheDocument();
+    const title = getByText(/TODO: show preloader/i);
+    expect(title).toBeInTheDocument();
+  });
 });
