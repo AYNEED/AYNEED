94c19c1e87dad330afb90c15aa33eb1684e1a76b
Kirill Neruchev <kirill@neruchev.com>
2020-07-15 21:57:36 +0200

    fix(app): fix app crashes

--- frontend-web/src/components/ui/forms/Notification.tsx
+++ frontend-web/src/components/ui/forms/Notification.tsx
@@ -1,21 +1,33 @@
 import React from 'react';
 import { ApolloError } from 'apollo-client';
 
+import { defaultDictionary } from 'shared';
 import { Msg } from 'src/i18n/Msg';
 
 type Props = {
   error?: ApolloError;
 };
 
-export const Notification: React.FC<Props> = ({ error }) =>
-  error ? (
+export const Notification: React.FC<Props> = ({ error }) => {
+  if (!error) {
+    return null;
+  }
+
+  return (
     <div>
       {!error.graphQLErrors.length && error.networkError && (
         <Msg id="error.system.network" />
       )}
 
-      {error.graphQLErrors.map(({ message }, i) => (
-        <Msg key={i + message} id={message as never} />
-      ))}
+      {error.graphQLErrors.map(({ message }, i) => {
+        const id = message as never;
+
+        if (defaultDictionary[id]) {
+          return <Msg key={i + message} id={id} />;
+        }
+
+        return message; // TODO: send Sentry event
+      })}
     </div>
-  ) : null;
+  );
+};
