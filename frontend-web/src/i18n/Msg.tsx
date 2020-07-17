65eadea41c508e3b5e6a6f7c16201db1ad414f26
seliana <cany96@bk.ru>
2020-07-17 23:14:21 +0300

    main(msg): fela removed from component

--- frontend-web/src/i18n/Msg.tsx
+++ frontend-web/src/i18n/Msg.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
 import { FormattedMessage, IntlShape as IIntlShape } from 'react-intl';
-import { FelaComponent } from 'react-fela';
 import { DictionaryKey } from 'shared';
-
-import { propsStyle } from 'src/utils/fela';
-
 
 export type IntlShape = IIntlShape;
 
 export interface MsgProps {
   id: DictionaryKey;
   values?: Record<string, React.ReactNode>;
-  style?: propsStyle
 }
 
 export const msg = (intl: IntlShape, { id, values }: MsgProps): string =>
   intl.formatMessage({ id }, values as never);
 
-export const Msg: React.FC<MsgProps> = ({ id, values, style = {} }) => ( 
-  <FelaComponent as="span" style={style}>
-    <FormattedMessage id={id} values={values} />
-  </FelaComponent>
+export const Msg: React.FC<MsgProps> = ({ id, values }) => (
+  <FormattedMessage id={id} values={values} />
 );
