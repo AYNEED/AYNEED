8950baac9aea5e18d92b3564e3c0ab70341b5052
seliana <cany96@bk.ru>
2020-07-17 23:13:44 +0300

    main(fela): render static moved to utils

--- frontend-web/src/components/App.tsx
+++ frontend-web/src/components/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { RendererProvider } from 'react-fela';
-import { createRenderer } from 'fela'
+
+import { renderer } from 'src/utils/fela';
 
 import { Router } from 'src/components/wrappers/Router';
 import { IntlProvider } from 'src/i18n/IntlProvider';
@@ -8,26 +9,6 @@ import { ApolloProvider } from 'src/components/wrappers/ApolloProvider';
 
 
 export const App: React.FC = () => {
-  const renderer = createRenderer()
-  renderer.renderStatic(`
-    * {
-      margin: 0;
-      padding: 0;
-      box-sizing: border-box;
-    }
-
-    body, html {
-      width: 100%;
-      height: 100%;
-    }
-
-    #root{
-      position: relative;
-      width: 100%;
-      height: 100%;
-    }
-  `)
-
 
   return(
     <ApolloProvider>
--- frontend-web/src/utils/fela.ts
+++ frontend-web/src/utils/fela.ts
@@ -31,8 +31,28 @@ export const renderer = createRenderer({
   ],
 });
 
+renderer.renderStatic(`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body, html {
+    width: 100%;
+    height: 100%;
+  }
+
+  #root{
+    position: relative;
+    width: 100%;
+    height: 100%;
+  }
+`)
+
 export type Styles<K extends string, T = {}, P = {}> = {
   [key in K]: CssFelaStyle<T, P>;
 };
 
-export type propsStyle = object
+// TODO: производную от CssFelaStyle 
+export type propsStyle = {}
