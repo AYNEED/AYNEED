a57b428d643eaaacff8893dc2c1bc8cba385468c
Kirill Neruchev <kirill@neruchev.com>
2020-07-25 10:57:49 +0200

    feat(feed): add beginning to client feed

---
+++ frontend-web/src/routes/Feed/FeedUsers.tsx
@@ -0,0 +1,98 @@
+import React, { useEffect, useCallback } from 'react';
+import { useQuery } from '@apollo/react-hooks';
+
+import {
+  GetUsersDocument,
+  GetUsersQuery,
+  OnUserAddedDocument,
+  OnUserAddedSubscription,
+  OnUserUpdatedDocument,
+  OnUserUpdatedSubscription,
+} from 'src/__generated__';
+import { Msg } from 'src/i18n/Msg';
+
+const CardUser = React.lazy(() => import('src/components/ui/CardUser'));
+
+export const FeedUsers: React.FC = () => {
+  const { error, data, fetchMore, subscribeToMore } = useQuery<GetUsersQuery>(
+    GetUsersDocument
+  );
+
+  useEffect(() => {
+    subscribeToMore<OnUserAddedSubscription>({
+      document: OnUserAddedDocument,
+      updateQuery: (prev, { subscriptionData }) => ({
+        ...prev,
+        users: {
+          ...prev.users,
+          items: [subscriptionData.data.userAdded, ...prev.users.items],
+        },
+      }),
+    });
+
+    subscribeToMore<OnUserUpdatedSubscription>({
+      document: OnUserUpdatedDocument,
+      updateQuery: (prev, { subscriptionData }) => ({
+        ...prev,
+        users: {
+          ...prev.users,
+          items: prev.users.items.map((user) =>
+            user.id === subscriptionData.data.userUpdated.id
+              ? subscriptionData.data.userUpdated
+              : user
+          ),
+        },
+      }),
+    });
+  }, [subscribeToMore]);
+
+  const loadMore = useCallback(() => {
+    if (data && data.users.hasMore) {
+      const { items } = data.users;
+
+      fetchMore({
+        query: GetUsersDocument,
+        variables: { cursor: items[items.length - 1].id },
+        updateQuery: (prev, { fetchMoreResult }) => {
+          if (!fetchMoreResult) {
+            return prev;
+          }
+
+          return {
+            ...prev,
+            users: {
+              ...prev.users,
+              ...fetchMoreResult.users,
+              items: [...prev.users.items, ...fetchMoreResult.users.items],
+            },
+          };
+        },
+      });
+    }
+  }, [fetchMore, data]);
+
+  if (!data) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <>
+      <h2>
+        <Msg id="web.routes.Feed.FeedUsers.title" />
+      </h2>
+
+      {error && <p>Error</p>}
+
+      {data.users.items.map((user) => (
+        <div key={user.id}>
+          <CardUser {...user} />
+
+          <hr />
+        </div>
+      ))}
+
+      {/* TODO: use auto-loading */}
+      <div onClick={loadMore}>load mode</div>
+    </>
+  );
+};
--- frontend-web/src/routes/Feed/index.tsx
+++ frontend-web/src/routes/Feed/index.tsx
@@ -1,87 +1,17 @@
-import React, { useEffect, useCallback } from 'react';
-import { useQuery } from '@apollo/react-hooks';
+import React from 'react';
 
 import { SearchForm } from 'src/components/blocks/SearchForm';
 import { Page } from 'src/components/wrappers/Page';
-import {
-  GetUsersDocument,
-  GetUsersQuery,
-  OnUserAddedDocument,
-  OnUserAddedSubscription,
-} from 'src/__generated__';
-import { Msg } from 'src/i18n/Msg';
+import { FeedBeginnings } from 'src/routes/Feed/FeedBeginnings';
+import { FeedUsers } from 'src/routes/Feed/FeedUsers';
 
-const CardUser = React.lazy(() => import('src/components/ui/CardUser'));
+const Feed: React.FC = () => (
+  <Page>
+    <SearchForm onSubmit={console.log} withChangeHistory />
 
-const Feed: React.FC = () => {
-  const { error, data, fetchMore, subscribeToMore } = useQuery<GetUsersQuery>(
-    GetUsersDocument
-  );
-
-  useEffect(() => {
-    subscribeToMore<OnUserAddedSubscription>({
-      document: OnUserAddedDocument,
-      updateQuery: (prev, { subscriptionData }) => ({
-        ...prev,
-        users: {
-          ...prev.users,
-          items: [subscriptionData.data.userAdded, ...prev.users.items],
-        },
-      }),
-    });
-  }, [subscribeToMore]);
-
-  const loadMore = useCallback(() => {
-    if (data && data.users.hasMore) {
-      const { items } = data.users;
-
-      fetchMore({
-        query: GetUsersDocument,
-        variables: { cursor: items[items.length - 1].id },
-        updateQuery: (prev, { fetchMoreResult }) => {
-          if (!fetchMoreResult) {
-            return prev;
-          }
-
-          return {
-            ...prev,
-            users: {
-              ...prev.users,
-              ...fetchMoreResult.users,
-              items: [...prev.users.items, ...fetchMoreResult.users.items],
-            },
-          };
-        },
-      });
-    }
-  }, [fetchMore, data]);
-
-  if (!data) {
-    return <p>Loading...</p>;
-  }
-
-  return (
-    <Page>
-      <h2>
-        <Msg id="web.routes.Feed.users" />
-      </h2>
-
-      <SearchForm onSubmit={console.log} withChangeHistory />
-
-      {error && <p>Error</p>}
-
-      {data.users.items.map((user) => (
-        <div key={user.id}>
-          <CardUser {...user} />
-
-          <hr />
-        </div>
-      ))}
-
-      {/* TODO: use auto-loading */}
-      <div onClick={loadMore}>load mode</div>
-    </Page>
-  );
-};
+    <FeedBeginnings />
+    <FeedUsers />
+  </Page>
+);
 
 export default Feed;
