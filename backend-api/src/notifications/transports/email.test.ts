15b1ca18f131b46eb8393d82da2bb6c08b08e836
andreyddk <7reyddk@gmail.com>
2020-07-29 08:28:03 +0700

    [ch363] Email notification

--- backend-api/src/notifications/transports/email.test.ts
+++ backend-api/src/notifications/transports/email.test.ts
@@ -1,47 +1,22 @@
+import { EVENTS } from 'src/notifications/events';
 import { email } from 'src/notifications/transports/email';
-import { IEmailTemplate } from 'src/notifications/types';
-
-const testEmail = '7reyddk@gmail.com';
-const testCode = '123456';
-
-const userAdded: IEmailTemplate = {
-  to: testEmail,
-  from: 'support@aynd.ru',
-  subject: 'Регистрация на AYNeed',
-  text: `
-    Благодарим Вас за интерес к платформе AYNeed!\n\n
-    Чтобы завершить регистрацию, перейдите по ссылке: ${
-      process.env.BASE_URL || 'localhost'
-    }/registerConfirm?email=${testEmail}&code=${testCode}\n\n
-    Ссылка действительна 10 минут
-    `,
-  html: `
-    Благодарим Вас за интерес к платформе AYNeed!<br><br>
-    Чтобы завершить регистрацию, перейдите <a href="${
-      process.env.BASE_URL || 'localhost'
-    }/registerConfirm?email=${testEmail}&code=${testCode}">перейдите по ссылке</a>.<br><br>
-    Ссылка действительна 10 минут
-    `,
-};
-
-const userForgotPassword: IEmailTemplate = {
-  to: testEmail,
-  from: 'support@aynd.ru',
-  subject: 'Восстановление пароля',
-  text: `Добрый день! Вы оставили заявку на сайте aynd.ru по смене пароля. Перейдите по ссылке, чтобы задать себе новый пароль: ${
-    process.env.BASE_CLIENT_URL || 'localhost'
-  }/passwordRecovery?email=${testEmail}&code=${testCode}`,
-  html: `Добрый день! Вы оставили заявку на сайте aynd.ru по смене пароля. <a href="${
-    process.env.BASE_CLIENT_URL || 'localhost'
-  }/passwordRecovery?email=${testEmail}&code=${testCode}">Нажмите сюда</a>, чтобы задать себе новый пароль`,
-};
 
 describe('Email notification', () => {
   test('Event USER_ADDED', () => {
-    expect(email({ event: 'USER_ADDED' }, userAdded));
+    expect(
+      email(
+        { event: EVENTS.USER_ADDED },
+        { to: '7reyddk@gmail.com', code: '123456' }
+      )
+    );
   });
 
   test('Event USER_FORGOT_PASSWORD', () => {
-    expect(email({ event: 'USER_FORGOT_PASSWORD' }, userForgotPassword));
+    expect(
+      email(
+        { event: EVENTS.USER_FORGOT_PASSWORD },
+        { to: '7reyddk@gmail.com', code: '123456' }
+      )
+    );
   });
 });
--- backend-api/src/notifications/transports/email.ts
+++ backend-api/src/notifications/transports/email.ts
@@ -1,8 +1,59 @@
+import sgMail, { MailDataRequired } from '@sendgrid/mail';
+
+import { EVENTS } from 'src/notifications/events';
 import { NotificationsBaseConfig } from 'src/notifications/types';
+
+export const userAddedEmailTpl = (payload: any): MailDataRequired => ({
+  to: payload.to,
+  from: 'support@aynd.ru',
+  subject: 'Регистрация на AYNeed',
+  text: `
+    Благодарим Вас за интерес к платформе AYNeed!\n\n
+    Чтобы завершить регистрацию, перейдите по ссылке: ${process.env.AYNEED_BACKEND_HOST_URL}/registerConfirm?email=${payload.to}&code=${payload.code}\n\n
+    Ссылка действительна 10 минут
+    `,
+  html: `
+    Благодарим Вас за интерес к платформе AYNeed!<br><br>
+    Чтобы завершить регистрацию, перейдите <a href="${process.env.AYNEED_BACKEND_HOST_URL}/registerConfirm?email=${payload.to}&code=${payload.code}">перейдите по ссылке</a>.<br><br>
+    Ссылка действительна 10 минут
+    `,
+});
+
+const userForgotPasswordEmailTpl = (payload: any): MailDataRequired => ({
+  to: payload.to,
+  from: 'support@aynd.ru',
+  subject: 'Восстановление пароля',
+  text: `Добрый день! Вы оставили заявку на сайте aynd.ru по смене пароля. Перейдите по ссылке, чтобы задать себе новый пароль: ${process.env.AYNEED_BACKEND_HOST_URL}/passwordRecovery?email=${payload.to}&code=${payload.code}`,
+  html: `Добрый день! Вы оставили заявку на сайте aynd.ru по смене пароля. <a href="${process.env.AYNEED_BACKEND_HOST_URL}/passwordRecovery?email=${payload.to}&code=${payload.code}">Нажмите сюда</a>, чтобы задать себе новый пароль`,
+});
 
 export const email = async <T extends {} = {}>(
   options: NotificationsBaseConfig,
   payload: T
 ): Promise<void> => {
-  console.log(options, payload);
+  sgMail.setApiKey(process.env.AYNEED_BACKEND_SENDGRID_API_KEY || '');
+
+  switch (options.event) {
+    case EVENTS.USER_ADDED:
+      try {
+        const mailData: MailDataRequired = userAddedEmailTpl(payload);
+        const res = await sgMail.send(mailData);
+        console.log(res);
+      } catch (error) {
+        console.log(error);
+      }
+      break;
+
+    case EVENTS.USER_FORGOT_PASSWORD:
+      try {
+        const mailData: MailDataRequired = userForgotPasswordEmailTpl(payload);
+        const res = await sgMail.send(mailData);
+        console.log(res);
+      } catch (error) {
+        console.log(error);
+      }
+      break;
+    default:
+      console.log('Notification event - not found');
+  }
 };
