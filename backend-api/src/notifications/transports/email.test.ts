83176362ca6811526f8aa9281cfb566973054588
andreyddk <7reyddk@gmail.com>
2020-07-30 07:32:03 +0700

    [ch363] Email notification (fix comments)

--- backend-api/src/notifications/transports/email.test.ts
+++ backend-api/src/notifications/transports/email.test.ts
@@ -2,18 +2,18 @@ import { EVENTS } from 'src/notifications/events';
 import { email } from 'src/notifications/transports/email';
 
 describe('Email notification', () => {
-  test('Event USER_ADDED - sending test email', () => {
+  test('Event USER_ADDED - sending test email', async () => {
     expect(
-      email(
+      await email(
         { event: EVENTS.USER_ADDED },
         { to: '7reyddk@gmail.com', code: '123456' }
       )
     );
   });
 
-  test('Event USER_FORGOT_PASSWORD - sending test email', () => {
+  test('Event USER_FORGOT_PASSWORD - sending test email', async () => {
     expect(
-      email(
+      await email(
         { event: EVENTS.USER_FORGOT_PASSWORD },
         { to: '7reyddk@gmail.com', code: '123456' }
       )
