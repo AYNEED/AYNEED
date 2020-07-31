72b02569c0c5750faf90d14c517af5c3d9a927aa
Kirill Neruchev <kirill@neruchev.com>
2020-07-31 21:02:38 +0200

    fix(transports): fix email transport

--- backend-api/src/notifications/transports/email.ts
+++ backend-api/src/notifications/transports/email.ts
@@ -1,58 +1,58 @@
 import sgMail, { MailDataRequired } from '@sendgrid/mail';
 
+import { ROUTES } from 'shared';
 import { EMAIL_SUPPORT } from 'src/constants';
 import { EVENTS } from 'src/notifications/events';
 import { NotificationsBaseConfig } from 'src/notifications/types';
 
-export const userAddedEmailTpl = (payload: any): MailDataRequired => ({
-  to: payload.to,
-  from: 'support@aynd.ru',
-  subject: 'Регистрация на AYNeed',
-  text: `
-    Благодарим Вас за интерес к платформе AYNeed!\n\n
-    Чтобы завершить регистрацию, перейдите по ссылке: ${process.env.AYNEED_BACKEND_HOST_URL}/registerConfirm?email=${payload.to}&code=${payload.code}
-    `,
-  html: `
-    Благодарим Вас за интерес к платформе AYNeed!<br><br>
-    Чтобы завершить регистрацию, перейдите <a href="${process.env.AYNEED_BACKEND_HOST_URL}/registerConfirm?email=${payload.to}&code=${payload.code}">перейдите по ссылке</a>.
-    `,
-});
+const webURL = process.env.AYNEED_BACKEND_HOST_URL;
 
-const userForgotPasswordEmailTpl = (payload: any): MailDataRequired => ({
-  to: payload.to,
-  from: EMAIL_SUPPORT,
-  subject: 'Восстановление пароля',
-  text: `Добрый день! Вы оставили заявку на сайте aynd.ru по смене пароля. Перейдите по ссылке, чтобы задать себе новый пароль: ${process.env.AYNEED_BACKEND_HOST_URL}/passwordRecovery?email=${payload.to}&code=${payload.code}`,
-  html: `Добрый день! Вы оставили заявку на сайте aynd.ru по смене пароля. <a href="${process.env.AYNEED_BACKEND_HOST_URL}/passwordRecovery?email=${payload.to}&code=${payload.code}">Нажмите сюда</a>, чтобы задать себе новый пароль`,
-});
+sgMail.setApiKey(process.env.AYNEED_BACKEND_SENDGRID_API_KEY || '');
+
+const eventToHandler = {
+  [EVENTS.BEGINNING_ADDED]: null,
+  [EVENTS.BEGINNING_UPDATED]: null,
+
+  [EVENTS.USER_ADDED]: ({ to, code }: any): MailDataRequired => ({
+    to,
+    from: EMAIL_SUPPORT,
+    subject: 'Регистрация на AYNeed',
+    text: `
+      Благодарим Вас за интерес к платформе AYNeed!\n\n
+      Чтобы завершить регистрацию, перейдите по ссылке: ${webURL}/registerConfirm?code=${code}
+    `,
+    html: `
+      Благодарим Вас за интерес к платформе AYNeed!<br><br>
+      Чтобы завершить регистрацию, перейдите <a href="${webURL}/registerConfirm?code=${code}">перейдите по ссылке</a>.
+    `,
+  }),
+  [EVENTS.USER_UPDATED]: null,
+
+  [EVENTS.USER_FORGOT_PASSWORD]: ({ to, code }: any): MailDataRequired => ({
+    to,
+    from: EMAIL_SUPPORT,
+    subject: 'Восстановление пароля',
+    text: `Добрый день! Вы оставили заявку на сайте aynd.ru для смены пароля. Перейдите по ссылке, чтобы задать себе новый пароль: ${webURL}${ROUTES.FORGOT_PASSWORD_CHANGE}?code=${code}`,
+    html: `Добрый день! Вы оставили заявку на сайте aynd.ru для смены пароля. <a href="${webURL}${ROUTES.FORGOT_PASSWORD_CHANGE}?code=${code}">Нажмите сюда</a>, чтобы задать себе новый пароль`,
+  }),
+};
 
 export const email = async <T extends {} = {}>(
   options: NotificationsBaseConfig,
   payload: T
 ): Promise<void> => {
-  sgMail.setApiKey(process.env.AYNEED_BACKEND_SENDGRID_API_KEY || '');
+  const handler = eventToHandler[options.event];
 
-  switch (options.event) {
-    case EVENTS.USER_ADDED:
-      try {
-        const mailData: MailDataRequired = userAddedEmailTpl(payload);
-        const res = await sgMail.send(mailData);
-        console.log(res);
-      } catch (error) {
-        console.log(error);
-      }
-      break;
-
-    case EVENTS.USER_FORGOT_PASSWORD:
-      try {
-        const mailData: MailDataRequired = userForgotPasswordEmailTpl(payload);
-        const res = await sgMail.send(mailData);
-        console.log(res);
-      } catch (error) {
-        console.log(error);
-      }
-      break;
-    default:
-      console.log('Notification event - not found');
+  if (handler) {
+    try {
+      const res = await sgMail.send(handler(payload));
+      console.log(res);
+    } catch (error) {
+      console.error(`[notifications email] ${error}`);
+    }
+  } else {
+    console.error(
+      `[notifications email] Event ${options.event} is not supported`
+    );
   }
 };
