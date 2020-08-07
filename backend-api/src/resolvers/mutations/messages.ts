aa209859ce5e7ee2b2b0d8c6ea93ddf05ae3553a
ilKhr <khorishko.ilja@yandex.ru>
2020-08-07 20:30:29 +0300

    Add check 'completeness' in addMessage

--- backend-api/src/resolvers/mutations/messages.ts
+++ backend-api/src/resolvers/mutations/messages.ts
@@ -7,7 +7,17 @@ export const addMessage: Resolvers['Mutation']['addMessage'] = async (
   parent,
   { text, authorId, recipientId }
 ) => {
-  findUserById(recipientId);
+  const recipient = await findUserById(recipientId);
+  const author = await findUserById(authorId);
+
+  if (
+    recipient.statistics.completeness !== 100 &&
+    author.statistics.completeness !== 100
+  ) {
+    throw new ValidationError('error.user.incompleteProfile');
+  }
+
+  //TODO: add exception (support chat)
 
   if (!text) {
     throw new ValidationError('error.message.empty');
