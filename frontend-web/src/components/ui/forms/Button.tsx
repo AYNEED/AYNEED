3ed639b0bf2e88ed7307c073a587106465fd3e52
seliana <cany96@bk.ru>
2020-08-14 20:23:32 +0300

    feat(button): border added

--- frontend-web/src/components/ui/forms/Button.tsx
+++ frontend-web/src/components/ui/forms/Button.tsx
@@ -6,6 +6,8 @@ import { Styles, PropsStyle } from 'src/utils/fela';
 import { Link } from 'src/components/ui/Link';
 import { Scheme } from 'src/navigation';
 import { COLOR } from 'src/constants/colors';
+import { FONT_SIZE, FONT_WEIGHT, font } from 'src/constants/fonts';
+
 
 
 type ButtonLinkProps = {
@@ -16,35 +18,39 @@ type ButtonSubmitProps = {
   disabled?: boolean;
 };
 
-const styles: Styles<'text'> = {
-  text: {
-    // color: COLOR.PRIMARY_500,
-    color: 'linear-gradient(90deg, #015496 0%, #09BDD0 100%) !important',
-    textTransform: 'uppercase',
-    fontStyle: 'normal',
-    fontWeight: 600,
-    fontSize: '16px',
-    lineHeight: '20px',
+const styles: Styles<'border'> = {
+  border: {
+    position: 'relative',
+    height: '50px',
+    minWidth: '200px',
+    padding: '2px',
+    backgroundColor: `${COLOR.GRADIENT_HORIZONTAL} !important`,
+    // color: 
+    borderRadius: '8px',
+
+    ':hover': {
+      background: `${COLOR.PRIMARY_300}`,
+      color: `${COLOR.PRIMARY_300}`
+    }
   },
 };
 
+
 const buttonRule: PropsStyle = () => ({
   position: 'relative',
-  height: '50px',
-  width: '200px',
-  backgroundColor: COLOR.TRANSPARENT,
-  border: '2px solid',
-  borderColor: 'linear-gradient(180deg, #09BDD0 0%, #015496 100%)',
+  height: '100%',
+  width: '100%',
+  padding: '2px',
+  backgroundColor: COLOR.WHITE,
+  // border: '2px solid',
+  // borderColor: 'linear-gradient(180deg, #09BDD0 0%, #015496 100%)',
   borderRadius: '8px',
-  // border: `2px solid ${COLOR.PRIMARY_500}`,
   display: 'flex !important',
   justifyContent: 'center',
   alignItems: 'center',
+  ...font(FONT_SIZE.L, FONT_WEIGHT.SEMIBOLD),
+  textTransform: 'uppercase',
   cursor: 'pointer',
-  ':hover': {
-    border: `2px solid ${COLOR.PRIMARY_300}`,
-    color: `${COLOR.PRIMARY_300}`
-  }
 })
 
 
@@ -53,7 +59,7 @@ export const ButtonLink: React.FC<ButtonLinkProps> = ({ url, children }) => {
   const buttonLink: React.RefObject<HTMLDivElement> = React.createRef()
 
 
-  const onMouseDownHandler = () => {
+  const tabHandler = () => {
     if (buttonLink.current !== null) {
       buttonLink.current.style.borderColor = `${COLOR.PRIMARY_200}`
       buttonLink.current.style.color = `${COLOR.PRIMARY_200}`
@@ -61,10 +67,12 @@ export const ButtonLink: React.FC<ButtonLinkProps> = ({ url, children }) => {
   }
   
   return(
-    <Link url={url} onMouseDown={onMouseDownHandler}>
-      <div className={css(buttonRule)} ref={buttonLink}>
-        <FelaComponent style={styles.text}>{children}</FelaComponent>
-      </div>
+    <Link url={url} onMouseDown={tabHandler}>
+      <FelaComponent style={styles.border}>
+        <div className={css(buttonRule)} ref={buttonLink}>
+          {children}
+        </div>
+      </FelaComponent>
     </Link>
   )
 };
