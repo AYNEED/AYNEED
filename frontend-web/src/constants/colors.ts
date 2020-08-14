faeb546525bb9ba87167e305198a4f586ea04d30
seliana <cany96@bk.ru>
2020-08-14 20:40:41 +0300

    test2

--- frontend-web/src/constants/colors.ts
+++ frontend-web/src/constants/colors.ts
@@ -1,32 +1,35 @@
 export enum COLOR {
-
-// primary colors
+  // primary colors
   PRIMARY_100 = '#015496',
   PRIMARY_200 = '#047CAC',
   PRIMARY_300 = '#09BCCF',
   PRIMARY_400 = '#C6EBEF',
   PRIMARY_500 = '#E7F3F5',
 
-// secondary colors
+  // secondary colors
   SECONDARY_100 = '#334D6E',
   SECONDARY_200 = '#86A3C6',
   SECONDARY_300 = '#C7D9EB',
   SECONDARY_400 = '#E4E9F0',
   SECONDARY_500 = '#FAFBFC',
 
-// gradient colors
+  // gradient colors
   GRADIENT_HORIZONTAL = 'linear-gradient(90deg, #015496 0%, #09BDD0 100%)',
   GRADIENT_VERTICAL_SUNSET = 'linear-gradient(180deg, #09BDD0 0%, #015496 100%)',
   GRADIENT_VERTICAL_DAWN = 'linear-gradient(180deg, #015496 0%, #09BDD0 100%)',
   GRADIENT = 'linear-gradient(90deg, #C6EBEF 0%, #E7F3F5 100%)',
 
-// others colors 
+  // others colors
   ERROR = '#ED5432',
   WHITE = '#FFFFFF',
   TRANSPARENT = 'transparent',
 
+  RED = '#EB5757', // удалить, если будет не нужен
+
+
+
+
+
 
   
-
-  RED = '#EB5757', // удалить, если будет не нужен
 }
