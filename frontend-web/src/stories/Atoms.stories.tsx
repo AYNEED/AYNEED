2c4b77f1de4b2d03dc68684b7137677bfa591fd4
Kirill Neruchev <kirill@neruchev.com>
2020-08-22 21:58:34 +0200

    feat(stories): add atoms story

---
+++ frontend-web/src/stories/Atoms.stories.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react/types-6-0';
+
+export default {
+  title: 'Documentation/Atoms',
+} as Meta;
+
+export const Badges: Story = () => <>TODO</>;
+
+export const Spinner: Story = () => <div className="spinner" />;
+
+export const Divider: Story = () => <>TODO</>;
+
+export const Time: Story = () => <>TODO</>;
+
+export const Date: Story = () => <>TODO</>;
