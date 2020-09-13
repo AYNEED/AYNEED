5cccdebe6b5612df0d1dba4b7075c78370678f7e
andrey.dudnik <7reyddk@gmail.com>
2020-09-13 20:34:31 +0700

    [ch457] Sign in email

--- frontend-web/src/components/blocks/EnterThrough.tsx
+++ frontend-web/src/components/blocks/EnterThrough.tsx
@@ -1,35 +1,73 @@
 import React from 'react';
 import { useIntl } from 'react-intl';
+import { FelaComponent } from 'react-fela'
 
+import { Styles } from 'src/utils/fela'
+import { COLOR } from 'src/constants/colors'
+import { font, FONT_SIZE, FONT_WEIGHT } from 'src/constants/fonts'
 import { Msg, msg } from 'src/i18n/Msg';
 import { Google } from 'src/components/icons/networks/Google';
 import { Facebook } from 'src/components/icons/networks/Facebook';
 import { VK } from 'src/components/icons/networks/VK';
+
+const styles: Styles<'root' | 'message' | 'list'> = {
+  root: {
+    display: 'flex',
+    marginBottom: 30,
+
+    nested: {
+      '> ul': {
+        display: 'flex',
+        listStyleType: 'none'
+      },
+    },
+  },
+  message: {
+    marginRight: 10,
+    color: COLOR.SECONDARY_200,
+    ...font(FONT_SIZE.M, FONT_WEIGHT.SEMIBOLD)
+  },
+  list: {
+    marginLeft: 10
+  }
+}
 
 export const EnterThrough: React.FC = () => {
   const intl = useIntl();
 
   return (
     <>
-      <Msg id="web.components.blocks.EnterThrough.title" />
+      <FelaComponent style={styles.root}>
+        <FelaComponent style={styles.message}>
+          <Msg id="web.components.blocks.EnterThrough.title" />
+        </FelaComponent>
 
-      <ul>
-        <li>
-          <a href="/" title={msg(intl, { id: 'networks.google' })}>
-            <Google />
-          </a>
-        </li>
-        <li>
-          <a href="/" title={msg(intl, { id: 'networks.facebook' })}>
-            <Facebook />
-          </a>
-        </li>
-        <li>
-          <a href="/" title={msg(intl, { id: 'networks.vk' })}>
-            <VK />
-          </a>
-        </li>
-      </ul>
+        <ul>
+          <FelaComponent style={styles.list}>
+            <li>
+              <a href="/" title={msg(intl, { id: 'networks.google' })}>
+                <Google fill={COLOR.SECONDARY_200} />
+              </a>
+            </li>
+          </FelaComponent>
+
+          <FelaComponent style={styles.list}>
+            <li>
+              <a href="/" title={msg(intl, { id: 'networks.facebook' })}>
+                <Facebook fill={COLOR.SECONDARY_200} />
+              </a>
+            </li>
+          </FelaComponent>
+
+          <FelaComponent style={styles.list}>
+            <li>
+              <a href="/" title={msg(intl, { id: 'networks.vk' })}>
+                <VK fill={COLOR.SECONDARY_200} />
+              </a>
+            </li>
+          </FelaComponent>
+        </ul>
+      </FelaComponent>
     </>
   );
 };
--- frontend-web/src/routes/SignInEmail/index.tsx
+++ frontend-web/src/routes/SignInEmail/index.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 import { useFormik } from 'formik';
 import { Msg } from 'src/i18n/Msg';
 import { useMutation } from '@apollo/client';
+import { FelaComponent } from 'react-fela'
 
+import { Styles } from 'src/utils/fela'
+import { font, FONT_SIZE, FONT_WEIGHT } from 'src/constants/fonts'
 import { COLOR } from 'src/constants/colors';
 import { Notification } from 'src/components/ui/forms/Notification';
 import { Page } from 'src/components/wrappers/Page';
@@ -17,6 +20,38 @@ import {
   SignInEmailMutationVariables,
 } from 'src/__generated__';
 import { EnterThrough } from 'src/components/blocks/EnterThrough';
+
+const styles: Styles<'root' | 'buttonSubmit' | 'link'> = {
+  root: () => ({
+    nested: {
+      '> input': {
+        display: 'block',
+        width: 260,
+        marginBottom: 40,
+        paddingBottom: 5,
+        border: 0,
+        borderBottom: `2px solid ${COLOR.SECONDARY_200}`,
+
+        '::placeholder': {
+          color: COLOR.SECONDARY_200,
+          ...font(FONT_SIZE.M, FONT_WEIGHT.REGULAR),
+        },
+
+        ':hover': {
+          borderColor: COLOR.SECONDARY_300,
+        },
+      },
+    }
+  }),
+  buttonSubmit: {
+    display: 'flex',
+    justifyContent: 'center',
+    marginBottom: 40
+  },
+  link: {
+    marginBottom: 10
+  }
+}
 
 const SignInEmail: React.FC = () => {
   const [signInEmail, result] = useMutation<
@@ -40,45 +75,53 @@ const SignInEmail: React.FC = () => {
       <EnterThrough />
 
       <form onSubmit={formik.handleSubmit}>
-        <Notification error={result.error} />
+        <FelaComponent style={styles.root}>
+          <Notification error={result.error} />
 
-        <InputEmail
-          name="email"
-          value={formik.values.email}
-          error={formik.errors.email}
-          onChange={formik.handleChange}
-          placeholder={{ id: 'web.routes.SignInEmail.form_email' }}
-        />
+          <InputEmail
+            name="email"
+            value={formik.values.email}
+            error={formik.errors.email}
+            onChange={formik.handleChange}
+            placeholder={{ id: 'web.routes.SignInEmail.form_email' }}
+          />
 
-        <InputPassword
-          name="password"
-          value={formik.values.password}
-          error={formik.errors.password}
-          onChange={formik.handleChange}
-          placeholder={{ id: 'web.routes.SignInEmail.form_password' }}
-        />
+          <InputPassword
+            name="password"
+            value={formik.values.password}
+            error={formik.errors.password}
+            onChange={formik.handleChange}
+            placeholder={{ id: 'web.routes.SignInEmail.form_password' }}
+          />
 
-        <ButtonSubmit>
-          <Msg id="web.routes.SignInEmail.form_submit" />
-        </ButtonSubmit>
+          <FelaComponent style={styles.buttonSubmit}>
+            <ButtonSubmit>
+              <Msg id="web.routes.SignInEmail.form_submit" />
+            </ButtonSubmit>
+          </FelaComponent>
+        </FelaComponent>
       </form>
 
       <p>
-        <Link
-          url={{ scheme: ROUTES.FORGOT_PASSWORD }}
-          color={COLOR.SECONDARY_200}
-        >
-          <Msg id="web.routes.SignInEmail.link_forgot_password" />
-        </Link>
+        <FelaComponent style={styles.link}>
+          <Link
+            url={{ scheme: ROUTES.FORGOT_PASSWORD }}
+            color={COLOR.SECONDARY_200}
+          >
+            <Msg id="web.routes.SignInEmail.link_forgot_password" />
+          </Link>
+        </FelaComponent>
       </p>
 
       <p>
-        <Link
-          url={{ scheme: ROUTES.SIGN_UP_EMAIL }}
-          color={COLOR.SECONDARY_200}
-        >
-          <Msg id="web.routes.SignInEmail.link_sign_up" />
-        </Link>
+        <FelaComponent style={styles.link}>
+          <Link
+            url={{ scheme: ROUTES.SIGN_UP_EMAIL }}
+            color={COLOR.SECONDARY_200}
+          >
+            <Msg id="web.routes.SignInEmail.link_sign_up" />
+          </Link>
+        </FelaComponent>
       </p>
     </Page>
   );
