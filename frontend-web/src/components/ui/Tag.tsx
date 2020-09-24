01977efd2b536a1ea6df89e9108e6383dd87ea1c
Anton <antoniocra04@gmail.com>
2020-09-24 21:18:07 +0600

    Add closeCallback, hover and blur effects

--- frontend-web/src/components/ui/Tag.tsx
+++ frontend-web/src/components/ui/Tag.tsx
@@ -3,11 +3,12 @@ import { FelaComponent } from 'react-fela';
 import { Styles } from 'src/utils/fela';
 import { COLOR } from 'src/constants/colors';
 import { font, FONT_SIZE, FONT_WEIGHT } from 'src/constants/fonts';
-import { Close } from 'src/components/icons/interactions/Close';
+import { CloseMini } from 'src/components/icons/interactions/CloseMini';
 
 export interface ITag {
   isActive: boolean;
   tagText: string;
+  closeCallback?: never;
 }
 
 const style: Styles<'tag' | 'tagActive' | 'tagText'> = {
@@ -16,10 +17,11 @@ const style: Styles<'tag' | 'tagActive' | 'tagText'> = {
     justifyContent: 'center',
     alignItems: 'center',
     width: 'auto',
-    height: 'auto',
+    height: '25px',
     padding: '5px 9px',
     border: `2px ${COLOR.SECONDARY_400} solid`,
     borderRadius: '50px',
+    color: COLOR.PRIMARY_100,
   },
 
   tagActive: {
@@ -27,20 +29,59 @@ const style: Styles<'tag' | 'tagActive' | 'tagText'> = {
     justifyContent: 'center',
     alignItems: 'center',
     width: 'auto',
-    height: 'auto',
+    height: '25px',
     padding: '5px 9px',
     border: `2px ${COLOR.SECONDARY_300} solid`,
     borderRadius: '50px',
     columnGap: '8px',
+    cursor: 'pointer',
+    color: COLOR.PRIMARY_100,
+    transition: 'all .2s ease',
+    nested: {
+      ':hover': {
+        borderColor: COLOR.PRIMARY_300,
+        color: COLOR.PRIMARY_300,
+      },
+    },
   },
 
   tagText: {
     ...font(FONT_SIZE.S, FONT_WEIGHT.MEDIUM),
-    color: COLOR.SECONDARY_100,
+    color: 'inherit',
   },
 };
 
 export const Tag: React.FC<ITag> = (props) => {
+  const [closeButtonColor, setCloseButtonColor] = React.useState(
+    COLOR.SECONDARY_300
+  );
+
+  function onHoverCloseButton(): void {
+    setCloseButtonColor(COLOR.PRIMARY_300);
+    style.tagActive = {
+      ...style.tagActive,
+      nested: {
+        ':hover': {
+          borderColor: COLOR.SECONDARY_300,
+          color: COLOR.PRIMARY_100,
+        },
+      },
+    };
+  }
+
+  function onBlurCloseButton(): void {
+    setCloseButtonColor(COLOR.SECONDARY_300);
+    style.tagActive = {
+      ...style.tagActive,
+      nested: {
+        ':hover': {
+          borderColor: COLOR.PRIMARY_300,
+          color: COLOR.PRIMARY_300,
+        },
+      },
+    };
+  }
+
   return (
     <FelaComponent
       style={props.isActive === true ? style.tagActive : style.tag}
@@ -50,7 +91,13 @@ export const Tag: React.FC<ITag> = (props) => {
         {props.tagText}
       </FelaComponent>
       {props.isActive === true ? (
-        <Close fill={COLOR.SECONDARY_300} width={15} height={15}></Close>
+        <div
+          onMouseOut={onBlurCloseButton}
+          onMouseOver={onHoverCloseButton}
+          onClick={props.closeCallback}
+        >
+          <CloseMini fill={closeButtonColor}></CloseMini>
+        </div>
       ) : null}
     </FelaComponent>
   );
