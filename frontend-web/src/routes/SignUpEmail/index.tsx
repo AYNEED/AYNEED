02e43ee080e48c262e9f24284a83546e0b8a5ea7
Anton <antoniocra04@gmail.com>
2020-10-02 17:51:54 +0600

    Create sign up page

--- frontend-web/src/routes/SignUpEmail/index.tsx
+++ frontend-web/src/routes/SignUpEmail/index.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { useFormik } from 'formik';
 import { Msg } from 'src/i18n/Msg';
 import { useMutation } from '@apollo/client';
-
+import { FelaComponent } from 'react-fela';
 import { Notification } from 'src/components/ui/forms/Notification';
+import { Styles } from 'src/utils/fela';
 import { Page } from 'src/components/wrappers/Page';
 import {
   InputText,
@@ -49,61 +50,77 @@ const SignUpEmail: React.FC = () => {
       }),
   });
 
+  const style: Styles<'container' | 'socialNetworksContainer'> = {
+    container: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      rowGap: '40px',
+      marginTop: '30px',
+    },
+
+    socialNetworksContainer: {
+      marginTop: '20px',
+    },
+  };
+
   return (
     <Page layout="entry" withTitle>
-      <EnterThrough />
-
       <form onSubmit={formik.handleSubmit}>
-        <Notification error={result.error} />
+        <FelaComponent style={style.container}>
+          <Notification error={result.error} />
 
-        <InputText
-          name="firstName"
-          value={formik.values.firstName}
-          error={formik.errors.firstName}
-          onChange={formik.handleChange}
-          placeholder={{ id: 'web.routes.SignUpEmail.form_first_name' }}
-        />
+          <InputText
+            name="firstName"
+            value={formik.values.firstName}
+            error={formik.errors.firstName}
+            onChange={formik.handleChange}
+            placeholder={{ id: 'web.routes.SignUpEmail.form_first_name' }}
+          />
 
-        <InputText
-          name="lastName"
-          value={formik.values.lastName}
-          error={formik.errors.lastName}
-          onChange={formik.handleChange}
-          placeholder={{ id: 'web.routes.SignUpEmail.form_last_name' }}
-        />
+          <InputText
+            name="lastName"
+            value={formik.values.lastName}
+            error={formik.errors.lastName}
+            onChange={formik.handleChange}
+            placeholder={{ id: 'web.routes.SignUpEmail.form_last_name' }}
+          />
 
-        <InputEmail
-          name="email"
-          value={formik.values.email}
-          error={formik.errors.email}
-          onChange={formik.handleChange}
-          placeholder={{ id: 'web.routes.SignUpEmail.form_email' }}
-        />
+          <InputEmail
+            name="email"
+            value={formik.values.email}
+            error={formik.errors.email}
+            onChange={formik.handleChange}
+            placeholder={{ id: 'web.routes.SignUpEmail.form_email' }}
+          />
 
-        <InputPassword
-          name="password"
-          value={formik.values.password}
-          error={formik.errors.password}
-          onChange={formik.handleChange}
-          placeholder={{ id: 'web.routes.SignUpEmail.form_password' }}
-        />
+          <InputPassword
+            name="password"
+            value={formik.values.password}
+            error={formik.errors.password}
+            onChange={formik.handleChange}
+            placeholder={{ id: 'web.routes.SignUpEmail.form_password' }}
+          />
 
-        <InputCheckbox
-          name="isAgree"
-          label={{ id: 'web.routes.SignUpEmail.form_is_agree' }}
-          value=""
-          checked={formik.values.isAgree}
-          onChange={formik.handleChange}
-        />
+          <InputCheckbox
+            name="isAgree"
+            label={{ id: 'web.routes.SignUpEmail.form_is_agree' }}
+            value=""
+            checked={formik.values.isAgree}
+            onChange={formik.handleChange}
+          />
 
-        <Button
-          disabled={!formik.values.isAgree}
-          text={{ id: 'web.routes.SignUpEmail.form_submit' }}
-          type="submit"
-          mode="origin"
-        />
+          <Button
+            disabled={!formik.values.isAgree}
+            text={{ id: 'web.routes.SignUpEmail.form_submit' }}
+            type="submit"
+            mode="origin"
+          />
+        </FelaComponent>
       </form>
-
+      <FelaComponent style={style.socialNetworksContainer}>
+        <EnterThrough />
+      </FelaComponent>
       <p>
         <Link url={{ scheme: ROUTES.SIGN_IN_EMAIL }} theme="negative">
           <Msg id="web.routes.SignUpEmail.link_sign_in" />
