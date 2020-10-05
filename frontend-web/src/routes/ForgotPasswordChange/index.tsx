06059263dba4f22dcd217423b818239b77b824bf
Anton <antoniocra04@gmail.com>
2020-10-05 14:58:42 +0600

    feat(Forgot password change page): Create forgot password change page without logic

--- frontend-web/src/routes/ForgotPasswordChange/index.tsx
+++ frontend-web/src/routes/ForgotPasswordChange/index.tsx
@@ -1,7 +1,43 @@
 import React from 'react';
+import { FelaComponent } from 'react-fela';
 
+import { Styles } from 'src/utils/fela';
 import { Page } from 'src/components/wrappers/Page';
+import { InputPassword } from 'src/components/ui/forms/Input';
+import { Button } from 'src/components/ui/forms/Button';
 
-const ForgotPasswordChange: React.FC = () => <Page layout="entry" withTitle />;
+const ForgotPasswordChange: React.FC = () => {
+  const style: Styles<'formContainer'> = {
+    formContainer: {
+      display: 'flex',
+      flexDirection: 'column',
+      rowGap: '40px',
+      marginTop: '30px',
+    },
+  };
+
+  return (
+    <Page layout="entry" withTitle>
+      <FelaComponent style={style.formContainer}>
+        <InputPassword
+          onChange={() => null}
+          name="password"
+          value={''}
+          placeholder={{ id: 'web.routes.SignInEmail.form_password' }}
+        ></InputPassword>
+        <InputPassword
+          onChange={() => null}
+          name="passwordRepeat"
+          value={''}
+          placeholder={{ id: 'web.routes.SignInEmail.form_password' }}
+        ></InputPassword>
+        <Button
+          mode="origin"
+          text={{ id: 'web.routes.ForgotPassword.form_submit' }}
+        ></Button>
+      </FelaComponent>
+    </Page>
+  );
+};
 
 export default ForgotPasswordChange;
