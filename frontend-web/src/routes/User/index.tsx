3671f335a6ea4eba9b969b29bf22dd346e28b0f8
Evgeniy <evg.ml.1995@gmail.com>
2020-10-11 17:02:54 +0300

    fix(user): eslint error onChange empty func

--- frontend-web/src/routes/User/index.tsx
+++ frontend-web/src/routes/User/index.tsx
@@ -128,7 +128,9 @@ const User: React.FC = () => {
               <InputTextArea
                 mode="display"
                 name=""
-                onChange={() => {}}
+                onChange={() => {
+                  // do nothing.
+                }}
                 placeholder={{ id: 'web.routes.Main.button_start' }}
                 value="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
               />
