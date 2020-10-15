94f3d0e00791791b2702ff5518067f91faf528fa
Anton <antoniocra04@gmail.com>
2020-10-15 19:54:39 +0600

    feat(Scroll bar): Create preview version of scrollbar and add it in story

---
+++ frontend-web/src/components/ui/Scrollbar.stories.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react/types-6-0';
+import { RendererProvider } from 'react-fela';
+import { renderer } from 'src/utils/fela';
+import {ScrollBarPreviewOnly} from 'src/components/ui/ScrollbarPreviewOnly'
+
+export default {
+    title: 'Atoms/ScrollBar',
+    component: ScrollBarPreviewOnly,
+  } as Meta;
+  
+  const Template: Story = (args) => (
+    <RendererProvider renderer={renderer}>
+      <ScrollBarPreviewOnly {...args}></ScrollBarPreviewOnly>
+    </RendererProvider>
+  );
+  
+  export const Normal = Template.bind({});
+  Normal.args = {
+  };
