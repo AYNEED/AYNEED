14c4c57b2c6b8af177fef5d9b2666cfe4cc5e56c
Anton <antoniocra04@gmail.com>
2020-10-15 19:55:26 +0600

    fix(Prettier): Fix prettier error

--- frontend-web/src/components/ui/ScrollbarPreviewOnly.tsx
+++ frontend-web/src/components/ui/ScrollbarPreviewOnly.tsx
@@ -4,25 +4,25 @@ import { FelaComponent } from 'react-fela';
 import { Styles } from 'src/utils/fela';
 
 const style: Styles<'container' | 'content'> = {
-    container: {
-        width: '200px',
-        height: '300px',
-        overflowY: 'scroll',
-        backgroundColor: 'gray'
-    },
+  container: {
+    width: '200px',
+    height: '300px',
+    overflowY: 'scroll',
+    backgroundColor: 'gray',
+  },
 
-    content: {
-        width: '300px',
-        height: '1000px',
-        backgroundColor: 'white',
-        border: '5px #000 dashed'
-    }
-}
+  content: {
+    width: '300px',
+    height: '1000px',
+    backgroundColor: 'white',
+    border: '5px #000 dashed',
+  },
+};
 
 export const ScrollBarPreviewOnly: React.FC = () => {
-    return(
-        <FelaComponent style={style.container}>
-            <FelaComponent style={style.content}></FelaComponent>
-        </FelaComponent>
-    )
-}
+  return (
+    <FelaComponent style={style.container}>
+      <FelaComponent style={style.content}></FelaComponent>
+    </FelaComponent>
+  );
+};
