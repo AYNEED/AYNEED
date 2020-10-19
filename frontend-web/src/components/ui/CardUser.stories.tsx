d2ab1b36fef3a36ec43e77b37627bf95947c9861
Anton <antoniocra04@gmail.com>
2020-10-19 17:24:48 +0600

    feat(User Card): create story

--- frontend-web/src/components/ui/CardUser.stories.tsx
+++ frontend-web/src/components/ui/CardUser.stories.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { RendererProvider } from 'react-fela';
 import { Story, Meta } from '@storybook/react/types-6-0';
+import { Router as ReactRouter } from 'react-router-dom';
+import { IntlProvider } from 'src/i18n/IntlProvider';
 
 import { renderer } from 'src/utils/fela';
 import { CardUser } from 'src/components/ui/CardUser';
+import { history } from 'src/navigation/store';
 
 export default {
   title: 'Organisms/Cards/User card',
@@ -11,22 +14,37 @@ export default {
 } as Meta;
 
 const Template: Story = (args) => (
-  <RendererProvider renderer={renderer}>
-    <CardUser {...args}></CardUser>
-  </RendererProvider>
+  <IntlProvider>
+    <RendererProvider renderer={renderer}>
+      <ReactRouter history={history}>
+        <CardUser {...args}></CardUser>
+      </ReactRouter>
+    </RendererProvider>
+  </IntlProvider>
 );
+
+const User = {
+  id: '1',
+  personal: {
+    firstName: 'Ann',
+    lastName: 'Isharova',
+    isAgree: false,
+    photo:
+      'https://get.pxhere.com/photo/person-girl-woman-hair-photography-portrait-model-youth-fashion-blue-lady-hairstyle-smile-long-hair-face-dress-eye-head-skin-beauty-blond-photo-shoot-brown-hair-portrait-photography-108386.jpg',
+  },
+};
 
 export const Example = Template.bind({});
 Example.args = {
-  id: '0',
+  id: User.id,
   network: {
-    isOnline: false,
-    client: 'mobile',
+    isOnline: true,
+    client: 'Desktop',
   },
   about: {
     skills: ['asdf'],
   },
   personal: {
-    photo: false,
+    photo: User.personal.photo,
   },
 };
