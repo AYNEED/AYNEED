08cedc321e8f1e916d05e00846a8337d42874f88
Anton <antoniocra04@gmail.com>
2020-10-22 16:01:07 +0600

    feat(Button back): Add hover effect and additionalText

--- frontend-web/src/components/ui/ButtonBack.stories.tsx
+++ frontend-web/src/components/ui/ButtonBack.stories.tsx
@@ -1,22 +1,30 @@
 import React from 'react';
+
 import { RendererProvider } from 'react-fela';
 import { Story, Meta } from '@storybook/react/types-6-0';
 import { renderer } from 'src/utils/fela';
 import { ButtonBack } from 'src/components/ui/ButtonBack';
+import { IntlProvider, Props } from 'src/i18n/IntlProvider';
 
 export default {
   title: 'Molecules/Button back',
   component: ButtonBack,
 } as Meta;
 
-const Tamplate: Story = (args) => (
-  <RendererProvider renderer={renderer}>
-    <ButtonBack {...args}></ButtonBack>
-  </RendererProvider>
+const Tamplate: Story<Props> = (args) => (
+  <IntlProvider>
+    <RendererProvider renderer={renderer}>
+      <ButtonBack {...args}></ButtonBack>
+    </RendererProvider>
+  </IntlProvider>
 );
 
-export const Example = Tamplate.bind({});
-Example.args = {
-  isActive: false,
-  tagText: 'Example',
+export const WithText = Tamplate.bind({});
+WithText.args = {
+  additionalText: true,
+};
+
+export const WithoutText = Tamplate.bind({});
+WithoutText.args = {
+  additionalText: false,
 };
--- frontend-web/src/components/ui/ButtonBack.tsx
+++ frontend-web/src/components/ui/ButtonBack.tsx
@@ -3,7 +3,14 @@ import { FelaComponent } from 'react-fela';
 
 import { Styles } from 'src/utils/fela';
 import { COLOR } from 'src/constants/colors';
+import { HillHovered } from 'src/components/icons/interactions/HillHovered';
 import { Hill } from 'src/components/icons/interactions/Hill';
+import { font, FONT_SIZE, FONT_WEIGHT } from 'src/constants/fonts';
+import { Msg } from 'src/i18n/Msg';
+
+export type Props = {
+  additionalText: boolean;
+};
 
 const style: Styles<'container' | 'line' | 'hillContainer'> = {
   container: {
@@ -11,27 +18,67 @@ const style: Styles<'container' | 'line' | 'hillContainer'> = {
     position: 'fixed',
     alignItems: 'center',
     height: '100%',
+    nested: {
+      '>p': {
+        marginLeft: '13px',
+        ...font(FONT_SIZE.M, FONT_WEIGHT.REGULAR),
+        color: COLOR.SECONDARY_200,
+        cursor: 'pointer',
+      },
+    },
   },
 
-  line: {
+  line: ({ hover }: { hover: boolean }) => ({
     position: 'fixed',
     width: '5px',
     height: '100%',
-    backgroundColor: COLOR.PRIMARY_200,
-  },
+    backgroundColor: hover ? COLOR.SECONDARY_400 : COLOR.PRIMARY_200,
+    opacity: hover ? 0.7 : 1,
+    nested: {
+      ':hover': {
+        backgroundColor: COLOR.SECONDARY_400,
+      },
+    },
+  }),
 
   hillContainer: {
     marginLeft: '5px',
+    display: 'flex',
+    alignItems: 'center',
+    zIndex: 10,
   },
 };
 
-export const ButtonBack: React.FC = () => {
+export const ButtonBack: React.FC<Props> = (props) => {
+  const [isHover, setHover] = React.useState(false);
+
   return (
     <FelaComponent style={style.container}>
-      <FelaComponent style={style.line}></FelaComponent>
-      <FelaComponent style={style.hillContainer}>
-        <Hill></Hill>
-      </FelaComponent>
+      <div
+        onMouseOver={() => setHover(true)}
+        onMouseOut={() => setHover(false)}
+        style={{ position: 'fixed', height: '100%' }}
+      >
+        <FelaComponent hover={isHover} style={style.line}></FelaComponent>
+      </div>
+      <div
+        onMouseOver={() => setHover(true)}
+        onMouseOut={() => setHover(false)}
+      >
+        <FelaComponent style={style.hillContainer}>
+          {isHover ? <HillHovered></HillHovered> : <Hill></Hill>}
+        </FelaComponent>
+      </div>
+      {props.additionalText ? (
+        <p
+          onMouseOver={() => setHover(true)}
+          onMouseOut={() => setHover(false)}
+        >
+          <Msg id={'web.components.ui.ButtonBack.back'}></Msg>
+        </p>
+      ) : (
+        ''
+      )}
     </FelaComponent>
   );
 };
