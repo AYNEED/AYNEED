aad3a8f00e88831ea8cd12653ea149659ffc10a9
andreyddk <7reyddk@gmail.com>
2020-11-05 10:52:43 +0700

    [ch374] Clear

--- frontend-web/src/resolvers/customResolvers.ts
+++ frontend-web/src/resolvers/customResolvers.ts
@@ -2,11 +2,9 @@ import { Query } from 'src/generated/state';
 
 export const isLoggedInResolver = {
   read(isLoggedIn: Query['isLoggedIn']): Query['isLoggedIn'] {
-    console.log('read');
     return isLoggedIn;
   },
   merge(isLoggedIn: Query['isLoggedIn']): Query['isLoggedIn'] {
-    console.log('write');
     return isLoggedIn;
   },
 };
